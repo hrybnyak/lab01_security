878e85350e11b43d7785a4c4fcb52efcbe37a2f2
Yelyzaveta Hrybniak <y.s.hrybniak@globallogic.com>
2021-12-01 16:28:38 +0200

    convert from base64

--- lab01_security/Program.cs
+++ lab01_security/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace lab01_security
 {
@@ -6,7 +9,26 @@ namespace lab01_security
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = File.ReadAllTextAsync($"{Environment.CurrentDirectory}/resources/input.txt").Result;
+            var bytes = GetBytesFromBinaryString(text);
+            var base64Text = Encoding.ASCII.GetString(bytes);
+            var convertFromBase64Text = Convert.FromBase64String(base64Text);
+            var convertedText = Encoding.ASCII.GetString(convertFromBase64Text);
+            Console.WriteLine(convertedText);
+        }
+
+        public static Byte[] GetBytesFromBinaryString(String binary)
+        {
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                String t = binary.Substring(i, 8);
+
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
         }
     }
 }
